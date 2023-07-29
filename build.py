03ed6f09675db412439b8f8bbf177953d6fdb3d7
Jacob Wujciak-Jens <jacob@wujciak.de>
2023-07-29 06:20:03 +0200

    run antlr4 through poetry

--- build.py
+++ build.py
@@ -3,7 +3,9 @@ import subprocess
 
 def generate_parser(setup_kwargs):
     print("Generating CMake Parser:")
-    subprocess.run(['antlr4', '-o', 'cmake_refactor/parser', 'CMake.g4'])
+    subprocess.run(
+        ["poetry", "run", "antlr4", "-o", "cmake_refactor/parser", "CMake.g4"]
+    )
     return setup_kwargs
 
 
